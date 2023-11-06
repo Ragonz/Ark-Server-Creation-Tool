f0ee34de2b0f724a9831fe33f4979bbdcd92f2f8
James Rimmer <therimmer96@gmail.com>
2023-11-06 16:49:19 +0000

    Fixed issue with IP address validation

--- ASA-Manager/ConfigurationWindow.xaml.cs
+++ ASA-Manager/ConfigurationWindow.xaml.cs
@@ -38,6 +38,13 @@ namespace ARKServerCreationTool
 
         private void btn_saveConfig_Click(object sender, RoutedEventArgs e)
         {
+            string ipAddressPattern = "(^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$)|((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))";
+            if (!Regex.Match(txt_IPAddress.Text, ipAddressPattern).Success)
+            {
+                MessageBox.Show("Entered IP address is not valid.");
+                return;
+            }
+
             ((ASCTConfiguration)Application.Current.Properties["globalConfig"]).GameDirectory = txt_gameDir.Text.Trim();
             ((ASCTConfiguration)Application.Current.Properties["globalConfig"]).useCustomLaunchArgs = chk_overrideLaunchArgs.IsChecked.Value;
             ((ASCTConfiguration)Application.Current.Properties["globalConfig"]).customLaunchArgs = txt_customLaunchArgs.Text.Trim();
